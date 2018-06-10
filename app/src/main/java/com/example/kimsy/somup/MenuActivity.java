965fcbb965be07a3157511898dae65cf0787f04a
yeomsh <sanghee2911@gmail.com>
2018-06-10 18:29:45 +0900

    다시

--- app/src/main/java/com/example/kimsy/somup/MenuActivity.java
+++ app/src/main/java/com/example/kimsy/somup/MenuActivity.java
@@ -20,7 +20,6 @@ public class MenuActivity extends AppCompatActivity {
 
     public void onBackClicked(View v){
         //fireBase로 데이터 저장하기
-        //회원가입 장면에서 회원가입 버튼 눌렀을 때 반응
         etEmail=findViewById(R.id.id);
         String id=etEmail.getText().toString();
         etPassword=findViewById(R.id.pw);
